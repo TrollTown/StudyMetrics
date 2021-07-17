3efea0dfb9dcac10a1e067459943627a8cab6390
ProfLayton2000 <cunananm2000@gmail.com>
2021-07-17 19:43:31 +1000

    finished radar summary function

---
+++ backend/clienttools.py
@@ -0,0 +1,41 @@
+from backend.dbtools import *
+from datetime import datetime
+import time
+
+def radarGraphForStudent(studentID,searchVal,searchMode):# or module or submodule
+	history = getHistoryByStudentID(studentID)
+	groups = {}
+	for qID,sID,fTime,sAns,res,approved in history:
+		if not approved:
+			continue
+		qData = getQuestionByID(qID)
+		qDiff = qData[8]
+		key = ''
+		if searchMode == 'subject' and qData[1]==searchVal:
+			key = getSubjectNameByID(qData[2]) #if searhcing by subject, split on module
+		elif searchMode == 'module'  and qData[2]==searchVal:
+			key = getSubjectNameByID(qData[3]) #if searhcing by module, split on submodule
+		if key not in groups:
+			groups[key] = []
+
+		fTime = int(datetime.strptime(fTime, '%Y-%m-%d %I:%M:%S %p').strftime('%s'))
+		
+		groups[key].append((res,qDiff,fTime))
+
+	currTime = int(time.time())
+
+	retVal = {}
+	for key,data in groups.items():
+		elo = 5
+		for res,diff,fTime in data:
+			days = (currTime-fTime)//(24*60*60) + 1
+			# if they get it right
+			if res:
+				elo = min(10,elo + max(0.05,(diff-elo)/5) * max(1,7.0/days))
+			else:
+				elo = max(0,elo - max(0.05,(elo-diff)/8) * max(1,7.0/days))
+				# if they get it wrong
+			# print(elo,res,diff,days)
+		retVal[key] = elo
+	return retVal
+
--- backend/dbtools.py
+++ backend/dbtools.py
@@ -1,56 +1,89 @@
 def getSubjectIDByName(subjectName):
-    cur.execute('''
-      select subjectID
-      from Subjects
-      where subjectID=%s
-    ''',[subjectName])
+	cur.execute('''
+		select subjectID
+		from Subjects
+		where subjectName=%s
+	''',[subjectName])
+
+def getSubjectNameByID(subjectID):
+	cur.execute('''
+		select subjectName
+		from Subjects
+		where subjectID=%s
+	''',[subjectID])
 
 def getModuleIDByName(moduleName):
-    cur.execute('''
-      select moduleID
-      from Modules
-      where moduleID=%s
-    ''',[moduleName])
+	cur.execute('''
+		select moduleID
+		from Modules
+		where moduleName=%s
+	''',[moduleName])
+
+def getModuleNameByID(moduleID):
+	cur.execute('''
+		select moduleName
+		from Modules
+		where moduleID=%s
+	''',[moduleID])
 
 def getSubmoduleIDByName(submoduleName):
-    cur.execute('''
-      select submoduleID
-      from Submodules
-      where submoduleID=%s
-    ''',[submoduleName])
+	cur.execute('''
+		select submoduleID
+		from Submodules
+		where submoduleName=%s
+	''',[submoduleName])
+
+def getSubmoduleNameByID(submoduleID):
+	cur.execute('''
+		select submoduleName
+		from Submodules
+		where submoduleID=%s
+	''',[submoduleID])
 
 def getQuestionByID(questionID):
-    cur.execute('''
-      select *
-      from Questions
-      where questionID=%s
-    ''',[questionID])
+	cur.execute('''
+		select *
+		from Questions
+		where questionID=%s
+	''',[questionID])
 
 def getHistoryByStudentID(studentID):
-    cur.execute('''
-      select *
-      from History
-      where studentID=%s
-      order by finish_time
-    ''',[studentID])
+	cur.execute('''
+		select *
+		from History
+		where studentID=%s
+		order by finish_time
+	''',[studentID])
 
 def getQuestionsBySubject(subjectID):
-    cur.execute('''
-      select *
-      from Questions
-      where subjectID=%s
-    ''',[subjectID])
-    
+	cur.execute('''
+		select *
+		from Questions
+		where subjectID=%s
+	''',[subjectID])
+	
 def getQuestionsByModule(moduleID):
-    cur.execute('''
-      select *
-      from Questions
-      where moduleID=%s
-    ''',[moduleID])
+	cur.execute('''
+		select *
+		from Questions
+		where moduleID=%s
+	''',[moduleID])
 
 def getQuestionsBySubmodule(submoduleID):
-    cur.execute('''
-      select *
-      from Questions
-      where submoduleID=%s
-    ''',[submoduleID])
+	cur.execute('''
+		select *
+		from Questions
+		where submoduleID=%s
+	''',[submoduleID])
+
+def addHistory(qID,sID,fTime,ans,res,approved):
+	cur.execute('''
+		insert into History
+		values (%s,%s,%s,%s,%s,%s)
+	''',[qID,sID,fTime,ans,res,approved])
+
+def addUser(email,name,passwd,isTeacher):
+	cur.execute('''
+		insert into History (email, name, passwd, isTeacher)
+		values (%s,%s,%s,%s,%s,%s)
+	''',[email,name,passwd,isTeacher])
