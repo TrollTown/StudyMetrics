6824917e177d4abc0b46ccf97a5c3f09a4175291
Deren Sozer <deren.sozer@gmail.com>
2021-07-17 18:28:44 +1000

    Added skeleton register page

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,11 +1,13 @@
 import { Button, Heading, HStack, Input, VStack } from "@chakra-ui/react";
 
-export default function Login() {
+export default function Register() {
   return (
     <VStack w="100vw" h="100vh" justify="center" spacing="6">
-      <Heading>Login</Heading>
-      <Input placeholder="Username" w="400px" m="4" />
+      <Heading>Register</Heading>
+      <Input placeholder="Name" w="400px" m="4" />
+      <Input placeholder="Email" w="400px" m="4" />
       <Input type="password" placeholder="Password" w="400px" m="4" />
+      <Input type="password" placeholder="Confirm Password" w="400px" m="4" />
       <HStack>
         <Button colorScheme="blue">Login</Button>
         <Button colorScheme="teal">Register</Button>
