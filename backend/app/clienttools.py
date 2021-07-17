7e4b999aef5e57ac4b0d324c5eaf8c859cdafe40
TrollTown <williamyinanshen2009@gmail.com>
2021-07-18 07:30:28 +1000

    dkasdsakldjksaldjasdsajdklsa:

--- backend/app/clienttools.py
+++ backend/app/clienttools.py
@@ -2,6 +2,7 @@ from app.dbtools import *
 from app.history import *
 from datetime import datetime
 import time
+from app import flask_app
 
 def getQuestionDataByID(questionID):
 	qData = getQuestionByID(questionID)
@@ -131,6 +132,7 @@ def getNextQuestionID(studentID,submoduleID):
 			lastTime[qID] = int(fTime.strftime('%s'))
 	items = list(lastTime.items())
 	items.sort(key=lambda x:x[1])
+	flask_app.logger.error(items)
 	return {
 		'questionID': items[0][0]
 	}
