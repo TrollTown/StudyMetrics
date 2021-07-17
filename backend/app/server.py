cde593c212208c553f838492ed1298d10d2c9cf4
TrollTown <williamyinanshen2009@gmail.com>
2021-07-18 07:33:12 +1000

    deren change request

--- backend/app/server.py
+++ backend/app/server.py
@@ -55,7 +55,12 @@ def login():
         return jsonify({"result" : "failed", "reason" : "Login failed"}),400
     stored_hash = users[0][3]
     if pbkdf2_sha512.verify(password, stored_hash):
-        return jsonify({"result" : "success", "token" : users[0][0], "type" : users[0][4]}), 200
+        userType = users[0][4]
+        if userType:
+            t = "teacher"
+        else:
+            t = "student"
+        return jsonify({"result" : "success", "token" : users[0][0], "type" : t}), 200
     else:
         return jsonify({"result" : "failed", "reason" : "Login failed"}), 400
 
