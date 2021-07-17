f9d1e60e4067c0c3b4abc0682b0bbc37111e9508
TrollTown <williamyinanshen2009@gmail.com>
2021-07-18 08:37:32 +1000

    add math topics

--- backend/setup.sql
+++ backend/setup.sql
@@ -135,7 +135,9 @@ values
     (DEFAULT,1,'Conjugate Theorems'),
     (DEFAULT,1,'Exponential Form'),
     (DEFAULT,1,'Trigonometric Identities'),
-    (DEFAULT,2,'U-substitution');
+    (DEFAULT,2,'U-substitution'),
+    (DEFAULT,2,'Integration by Parts'),
+    (DEFAULT,2,'Partial Fraction Integration');
 
 insert into Questions
 values 
