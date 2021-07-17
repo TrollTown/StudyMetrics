78e5c0eef2cf5bf4b361571129891c071e5c639b
TrollTown <williamyinanshen2009@gmail.com>
2021-07-17 17:47:25 +1000

    refactor:

--- backend/run.py
+++ backend/run.py
@@ -1,23 +1,4 @@
 from app import flask_app
-from backend.whiteboard import save_canvas, load_canvas
 
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0',debug=True, port = 50000, use_reloader=True)
-
-#Nathan: code for saving students' whiteboard
-# what do i put for the url??
-@APP.route("/save_whiteboard", methods=['POST'])
-def get_canvas_from_web():
-    payload = request.get_json()
-    resp = save_canvas(payload['canvas_id'], payload['canvas_coordinates']) # save_canvas function saves to database
-    dump_data()
-    return dumps(resp)
-
-#Nathan: code for loading a saved whiteboard
-# what do i put for the url??
-@APP.route("/load_whiteboard", methods=['POST'])
-def load_canvas():
-    payload = request.get_json()
-    resp = load_canvas(payload['canvas_id']) # load_canvas grabs array from database
-    dump_data()
-    return dumps(resp)
