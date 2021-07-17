fb6d394c8833a3771ad5c24618d82ae61b766fca
TrollTown <williamyinanshen2009@gmail.com>
2021-07-18 04:22:31 +1000

    typo

--- backend/setup.sql
+++ backend/setup.sql
@@ -96,7 +96,7 @@ values
 
 insert into Teaches
 values 
-    (DEFAULT,1,"Year 12 Math 2021",1]),
+    (DEFAULT,1,"Year 12 Math 2021",1),
     (DEFAULT,1,"Year 11 Math 2021",6),
     (DEFAULT,1,"Year 10 Math 2021",6),
     (DEFAULT,1,"Year 9 Math 2021",6;
